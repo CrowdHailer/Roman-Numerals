3d3821ac86361d3aaee723d3f356594338296b5d
Peter Saxton <peterhsaxton@gmail.com>
2014-01-16 18:49:21 +0000

    Passed test for 40 and 50 Refactored

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,9 +1,10 @@
 def convert(numeral)
-	ones = numeral.scan(/I/).length
-	fives = numeral.scan(/V/).length
-	tens = numeral.scan(/X/).length
+	ones, fives, tens, fiftys = [/I/,/V/,/X/,/L/].map { |e| numeral.scan(e).length}
+
 	negative_ones = numeral.scan(/I[VX]/).length
+	negative_tens = numeral.scan(/XL/).length
 
 	ones *= (-1) if negative_ones != 0
-	(10*tens) + (5*fives) + ones
+	tens *= (-1) if negative_tens != 0
+	(50*fiftys) + (10*tens) + (5*fives) + ones
 end
