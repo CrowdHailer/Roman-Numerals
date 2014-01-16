2ad03b3e5d3950d350d28c13f51052e59f13a3dc
Peter Saxton <peterhsaxton@gmail.com>
2014-01-16 17:48:42 +0000

    Passed for numeral IX

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -3,6 +3,7 @@ def convert(numeral)
 	fives = numeral.scan(/V/).length
 	tens = numeral.scan(/X/).length
 
-	return 5 - ones if numeral == "IV"
+	return 5 * fives - ones if numeral == "IV"
+	return 10 * tens - ones if numeral == "IX"
 	return (10*tens) + (5*fives) + ones
 end
