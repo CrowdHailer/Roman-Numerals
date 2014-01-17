4599fc5bfcdc3f3f9c56adfee2dd14dcffbb6600
Peter Saxton <peterhsaxton@gmail.com>
2014-01-17 12:09:19 +0000

    Passed tests up to 100

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,12 +1,13 @@
 def convert(numeral)
-	ones, fives, tens, fiftys, hundreds, five_hundreds = [/I/,/V/,/X/,/L/,/C/,/D/].map { |e| numeral.scan(e).length}
+	ones, fives, tens, fiftys, hundreds, five_hundreds, thousands = [/I/,/V/,/X/,/L/,/C/,/D/,/M/].map { |e| numeral.scan(e).length}
 
 	negative_ones = numeral.scan(/I[VX]/).length
 	negative_tens = numeral.scan(/X[LC]/).length
-	negative_hundreds = numeral.scan(/C[D]/).length
+	negative_hundreds = numeral.scan(/C[DM]/).length
 
-	total = (500*five_hundreds) + (100*hundreds) + (50*fiftys) + (10*tens) + (5*fives) + ones
-	total = (negative_ones != 0) ? total -2 : total
-	total = (negative_tens != 0) ? total -20 : total
-	total = (negative_hundreds != 0) ? total -200 : total
+	total = (1000*thousands) +(500*five_hundreds) + (100*hundreds) + (50*fiftys) + (10*tens) + (5*fives) + ones
+	total += negative_ones * -2
+	total += negative_tens * -20
+	total += negative_hundreds * -200
+	
 end
