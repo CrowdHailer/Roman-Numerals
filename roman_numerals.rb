50d63e9172ef00efe8564a8b7505ab79969b180e
Peter Saxton <peterhsaxton@gmail.com>
2014-01-17 12:17:18 +0000

    Refactored to use inject to get to total

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,13 +1,5 @@
 def convert(numeral)
-	ones, fives, tens, fiftys, hundreds, five_hundreds, thousands = [/I/,/V/,/X/,/L/,/C/,/D/,/M/].map { |e| numeral.scan(e).length}
-
-	negative_ones = numeral.scan(/I[VX]/).length
-	negative_tens = numeral.scan(/X[LC]/).length
-	negative_hundreds = numeral.scan(/C[DM]/).length
-
-	total = (1000*thousands) +(500*five_hundreds) + (100*hundreds) + (50*fiftys) + (10*tens) + (5*fives) + ones
-	total += negative_ones * -2
-	total += negative_tens * -20
-	total += negative_hundreds * -200
-	
+	counts = [/I/,/V/,/X/,/L/,/C/,/D/,/M/,/I[VX]/,/X[LC]/,/C[DM]/].map { |e| numeral.scan(e).length}
+	values = [1,5,10,50,100,500,1000,-2,-20,-200]
+	(0..counts.size-1).inject(0) { |mem, var|  mem + counts[var]*values[var]}
 end
