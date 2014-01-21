480230e5a142f0288b29a839dbe27b2a7180a283
Peter Saxton <peterhsaxton@gmail.com>
2014-01-21 16:43:24 +0000

    Cleared Numerals

--- roman_numerals.rb
+++
@@ -1,5 +0,0 @@
-def convert(numeral)
-	counts = [/I/,/V/,/X/,/L/,/C/,/D/,/M/,/I[VX]/,/X[LC]/,/C[DM]/].map { |e| numeral.scan(e).length}
-	values = [1,5,10,50,100,500,1000,-2,-20,-200]
-	(0..counts.size-1).inject(0) { |mem, var|  mem + counts[var]*values[var]}
-end
