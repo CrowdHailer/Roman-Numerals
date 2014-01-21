ddb0d1d7c4f6cd9203b5d70b0ea179d3e0ed1a48
Peter Saxton <peterhsaxton@gmail.com>
2014-01-21 16:56:13 +0000

    Created rudimental Roman Numeral Class

---
+++ roman_numerals.rb
@@ -0,0 +1,14 @@
+class Roman
+	def initialize value
+		@value = value
+	end
+
+	def value
+		@value
+	end
+
+	def numeral
+		"I"*value
+	end
+
+end
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -1,1 +1,27 @@
+require_relative "../roman_numerals"
 
+describe Roman do
+
+	it "should represent the number 1 given 1" do
+		expect(Roman.new(1).value).to eq(1)		
+	end
+
+	it "should represent the number 2 given 2" do
+		expect(Roman.new(2).value).to eq(2)		
+	end
+
+	it "should return I when given 1" do
+		expect(Roman.new(1).numeral).to eq("I")		
+	end
+
+	it "should return II when object given 2" do
+		roman = Roman.new(2)
+		expect(roman.numeral).to eq("II")		
+	end
+
+	it "should return V when object given 5" do
+		roman = Roman.new(5)
+		expect(roman.numeral).to eq("V")		
+	end
+
+end
